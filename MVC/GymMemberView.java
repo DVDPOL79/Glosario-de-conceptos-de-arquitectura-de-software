0348556d0385a2a70e98d28cb24c94875319810a
David Alejandro Caceres Polania <72322125+DVDPOL79@users.noreply.github.com>
2024-09-13 16:34:47 -0500

    Create GymMemberView.java

---
+++ MVC/GymMemberView.java
@@ -0,0 +1,7 @@
+public class GymMemberView {
+    public void printGymMemberDetails(String memberName, String membershipType) {
+        System.out.println("Detalles de la membresia ");
+        System.out.println("Nombre: " + memberName);
+        System.out.println("Tipo de membresia: " + membershipType);
+    }
+}
